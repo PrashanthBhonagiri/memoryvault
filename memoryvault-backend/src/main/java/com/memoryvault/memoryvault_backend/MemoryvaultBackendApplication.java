10d37194fb5dc4bad7a7121ab77b0401070af3ae
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2024-12-21 10:53:06 +0530

    Initial Spring Boot project setup

---
+++ memoryvault-backend/src/main/java/com/memoryvault/memoryvault_backend/MemoryvaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.memoryvault.memoryvault_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryvaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryvaultBackendApplication.class, args);
+	}
+
+}
---
+++ memoryvault-backend/src/test/java/com/memoryvault/memoryvault_backend/MemoryvaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memoryvault.memoryvault_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryvaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
